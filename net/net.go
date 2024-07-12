93685b89436f41569c945ad591e8708ea3db2107
harley9293 <xuhanlin2012@gmail.com>
2024-07-12 16:30:02 +0800

    remove net.GetPubID

--- net/net.go
+++ net/net.go
@@ -1,29 +1,11 @@
 package net
 
 import (
-	"errors"
 	"fmt"
 	"golang.org/x/crypto/ssh"
 	"io"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"time"
 )
-
-// GetPublicIP Retrieve public IP address.
-func GetPublicIP() (string, error) {
-	c := http.Client{}
-	c.Timeout = time.Second * 10
-	rsp, err := c.Get("http://47.112.241.125:3001/ip")
-	if err != nil {
-		return "", errors.New("Failed to retrieve external IP. Please check the network.")
-	}
-	defer rsp.Body.Close()
-	body, _ := ioutil.ReadAll(rsp.Body)
-
-	return string(body), nil
-}
 
 // DownloadFile Download file from ssh.Session
 func DownloadFile(session *ssh.Session, remotePath, localPath string) error {
